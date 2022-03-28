b7320367dfc92e0fde945e8aecb6f88279585e20
chirazzzz <chiragmehta.uk@gmail.com>
2022-03-28 13:13:35 +0100

    added error state in case user has geolocation disabled

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  function checkGeoLocation() {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        console.log(position);
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+      },
+      function (error) {
+        console.error("Error Code = " + error.code + " - " + error.message);
+      }
+    );
+  }
 
-function checkGeoLocation() {
-  navigator.geolocation.getCurrentPosition((function(position) {
-    console.log("Latitude is :", position.coords.latitude);
-    console.log("Longitude is :", position.coords.longitude);
-  }))
-}
-  return (
-    <div className="App">
-      {checkGeoLocation()}
-    </div>
-  );
+  return <div className="App">{checkGeoLocation()}</div>;
 }
 
 export default App;
